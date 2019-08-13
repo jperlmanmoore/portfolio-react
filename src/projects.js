a56abc409ac312d6436866d0774882c8757a652c
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-08-13 12:13:42 -0400

    setup

---
+++ src/projects.js
@@ -0,0 +1,45 @@
+const cardData = [{
+    "id": 1,
+    "src": "./assets/images/bird.jpg",
+    "title": "Star Wars",
+    "link": "https://github.com/jperlmanmoore/unit-4-game"
+},
+{
+    "id": 2,
+    "src": "./assets/images/bananaman.jpg",
+    "title": "bananaman",
+    "link": "https://github.com/jperlmanmoore/liri-node-app"
+},
+{
+    "id": 3,
+    "src": "/assets/images/catlaugh.jpg",
+    "title": "giphy",
+    "link": "https://github.com/jperlmanmoore/giphy-api"
+},
+{
+    "id": 4,
+    "src": "./assets/images/catsphinx.jpg",
+    "title": "bamazon",
+    "link": "https://github.com/jperlmanmoore/bamazon-app"
+},
+{
+    "id": 5,
+    "src": "./assets/images/ladywithglassesmustache.jpg",
+    "title": "Eat Da Burger",
+    "link": "https://github.com/jperlmanmoore/edb"
+},
+{
+    "id": 6,
+    "src": "./assets/images/macaque.jpg",
+    "title": "clicky",
+    "link": "https://github.com/jperlmanmoore/clicky"
+},
+{
+    "id": 7,
+    "src": "./assets/images/smileyface.jpg",
+    "title": "mongo",
+    "link": "https://github.com/jperlmanmoore/clicky"
+}
+]
+
+export default cardData
